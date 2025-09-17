583d723c3b91f6cbca9dca15a8bc85ad22cfda13
ash1ra <ash1rawtf@gmail.com>
2025-09-17 14:38:48 +0300

    feat: create a train/val/test split; create a custom dataset class; create functions to save datasets aand get dataloaders

---
+++ data_loading.py
@@ -0,0 +1,85 @@
+from pathlib import Path
+
+import pandas as pd
+import torch
+from torch.utils.data import DataLoader, Dataset
+
+
+TRAIN_RATIO = 0.7
+VAL_RATIO = 0.15
+
+
+class TimeSeriesDataset(Dataset):
+    def __init__(
+        self,
+        data: pd.DataFrame,
+        target_col: str,
+        feature_cols: list[str],
+        seq_length: int,
+    ) -> None:
+        self.data = data[feature_cols].values
+        self.targets = data[target_col].values
+        self.seq_length = seq_length
+
+    def __len__(self) -> int:
+        return len(self.data) - self.seq_length
+
+    def __getitem__(self, idx) -> tuple[torch.Tensor]:
+        return (
+            torch.tensor(self.data[idx : idx + self.seq_length], dtype=torch.float32),
+            torch.tensor(self.targets[idx + self.seq_length], dtype=torch.float32),
+        )
+
+
+def split_data(data: pd.DataFrame) -> tuple[pd.DataFrame]:
+    total_size = len(data)
+    train_size = int(TRAIN_RATIO * total_size)
+    val_size = int(VAL_RATIO * total_size)
+
+    return (
+        data.iloc[:train_size],
+        data.iloc[train_size : train_size + val_size],
+        data.iloc[train_size + val_size :],
+    )
+
+
+def save_datasets(
+    output_dir: str,
+    train_ds: Dataset,
+    val_ds: Dataset,
+    test_ds: Dataset,
+) -> None:
+    output_path = Path(output_dir)
+
+    torch.save(train_ds, output_path / "train_ds.pt")
+    torch.save(val_ds, output_path / "val_ds.pt")
+    torch.save(test_ds, output_path / "test_ds.pt")
+
+
+def get_dataloaders(
+    train_ds_path, val_ds_path, test_ds_path, batch_size
+) -> tuple[DataLoader]:
+    train_ds = torch.load(train_ds_path, weights_only=False)
+    val_ds = torch.load(val_ds_path, weights_only=False)
+    test_ds = torch.load(test_ds_path, weights_only=False)
+
+    return (
+        DataLoader(train_ds, batch_size=batch_size, shuffle=False),
+        DataLoader(val_ds, batch_size=batch_size, shuffle=False),
+        DataLoader(test_ds, batch_size=batch_size, shuffle=False),
+    )
+
+
+path = Path("data")
+df = pd.read_csv(path / "processed_data.csv")
+
+train_df, val_df, test_df = split_data(df)
+
+target_col = "count"
+feature_cols = df.drop(target_col, axis=1).columns
+
+train_ds = TimeSeriesDataset(train_df, target_col, feature_cols, 3)
+val_ds = TimeSeriesDataset(val_df, target_col, feature_cols, 3)
+test_ds = TimeSeriesDataset(test_df, target_col, feature_cols, 3)
+
+save_datasets("data", train_ds, val_ds, test_ds)
