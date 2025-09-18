9d1755bf6ef6ce5e4804d1a280327b1f60b6b61f
ash1ra <ash1rawtf@gmail.com>
2025-09-18 14:36:28 +0300

    feat: create different utils functions and use it to train and test the model

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from pathlib import Path
+
+from torch import nn, optim
+
 from data_loading import get_dataloaders
+from model import RNNModel
+from utils import test, train, mape_metric
 
 
 def main() -> None:
@@ -13,9 +18,15 @@ def main() -> None:
         train_ds_path, val_ds_path, test_ds_path, 32
     )
 
-    print(train_dl, val_dl, test_dl)
-    for X, y in val_dl:
-        print(X, y)
+    input_size = train_dl.dataset.data[0].size
+    rnn_model = RNNModel(input_size, 1, 1)
+
+    epochs = 100
+    loss_fn = nn.MSELoss()
+    optimizer = optim.SGD(rnn_model.parameters(), lr=0.1)
+
+    train(rnn_model, train_dl, val_dl, loss_fn, mape_metric, optimizer, epochs)
+    test(rnn_model, test_dl, loss_fn, mape_metric)
 
 
 if __name__ == "__main__":
---
+++ utils.py
@@ -0,0 +1,102 @@
+from collections.abc import Callable
+
+import numpy as np
+import torch
+from torch import nn, optim
+from torch.utils.data import DataLoader
+
+
+def mape_metric(preds, targets):
+    preds = np.exp(preds.detach().cpu().numpy())
+    targets = np.exp(targets.detach().cpu().numpy())
+    return np.mean(np.abs((targets - preds) / targets)) * 100
+
+
+def train_step(
+    model: nn.Module,
+    train_dl: DataLoader,
+    loss_fn: nn.Module,
+    metric_fn: Callable,
+    optimizer: optim.Optimizer,
+    device: str = "cpu",
+) -> tuple[float]:
+    train_loss, train_metric = 0, 0
+    model.train()
+
+    for inputs, targets in train_dl:
+        inputs, targets = inputs.to(device), targets.to(device)
+
+        preds = model(inputs)
+
+        loss = loss_fn(preds, targets)
+
+        train_loss += loss.item()
+        train_metric += metric_fn(preds, targets)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    train_loss /= len(train_dl)
+    train_metric /= len(train_dl)
+
+    return train_loss, train_metric
+
+
+def test_step(
+    model: nn.Module,
+    test_dl: DataLoader,
+    loss_fn: nn.Module,
+    metric_fn: Callable,
+    device: str = "cpu",
+) -> tuple[float]:
+    test_loss, test_metric = 0, 0
+    model.eval()
+
+    with torch.inference_mode():
+        for inputs, targets in test_dl:
+            inputs, targets = inputs.to(device), targets.to(device)
+
+            preds = model(inputs)
+
+            loss = loss_fn(preds, targets)
+
+            test_loss += loss.item()
+            test_metric += metric_fn(preds, targets)
+
+    test_loss /= len(test_dl)
+    test_metric /= len(test_dl)
+
+    return test_loss, test_metric
+
+
+def train(
+    model: nn.Module,
+    train_dl: DataLoader,
+    val_dl: DataLoader,
+    loss_fn: nn.Module,
+    metric_fn: Callable,
+    optimizer: optim.Optimizer,
+    epochs: int,
+    device: str = "cpu",
+) -> None:
+    for epoch in range(1, epochs + 1):
+        train_loss, train_metric = train_step(
+            model, train_dl, loss_fn, metric_fn, optimizer, device
+        )
+        val_loss, val_metric = test_step(model, val_dl, loss_fn, metric_fn, device)
+
+        print(
+            f"Epoch: {epoch} | Train loss: {train_loss:.2f} | Train {metric_fn.__name__}: {train_metric:.2f}% | Val loss: {val_loss:.2f} | Val {metric_fn.__name__}: {val_metric:.2f}%"
+        )
+
+
+def test(
+    model: nn.Module,
+    data_loader: DataLoader,
+    loss_fn: nn.Module,
+    metric_fn: Callable,
+    device: str = "cpu",
+) -> None:
+    test_loss, test_metric = test_step(model, data_loader, loss_fn, metric_fn, device)
+    print(f"Test loss: {test_loss:.2f} | Test {metric_fn.__name__}: {test_metric:.2f}%")
